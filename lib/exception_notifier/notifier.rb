80a03d09a84c3e324211f6e02721f0bae15ce5c0
John Joseph Bachir <j@jjb.cc>
2011-11-03 23:43:59 -0400

    Allow the user to provide a custom message and hash of data when invoking.

--- lib/exception_notifier/notifier.rb
+++ lib/exception_notifier/notifier.rb
@@ -55,9 +55,9 @@ class ExceptionNotifier
       end
     end
 
-    def exception_notification(env, exception)
+    def exception_notification(env, exception, options={})
       self.append_view_path Rails.root.nil? ? "app/views" : "#{Rails.root}/app/views" if defined?(Rails)
-      
+
       @env        = env
       @exception  = exception
       @options    = (env['exception_notifier.options'] || {}).reverse_merge(self.class.default_options)
@@ -66,6 +66,8 @@ class ExceptionNotifier
       @backtrace  = exception.backtrace ? clean_backtrace(exception) : []
       @sections   = @options[:sections]
       data        = env['exception_notifier.exception_data'] || {}
+
+      process_custom(options[:custom_message], options[:custom_hash])
 
       data.each do |name, value|
         instance_variable_set("@#{name}", value)
@@ -77,7 +79,7 @@ class ExceptionNotifier
       end
     end
 
-    def background_exception_notification(exception, data={})
+    def background_exception_notification(exception, data={}, options={})
       if @notifier = Rails.application.config.middleware.detect{ |x| x.klass == ExceptionNotifier }
         @options = (@notifier.args.first || {}).reverse_merge(self.class.default_options)
         @exception = exception
@@ -90,6 +92,8 @@ class ExceptionNotifier
         end
         subject  = compose_subject(exception)
 
+        process_custom(options[:custom_message], options[:custom_hash])
+
         mail(:to => @options[:exception_recipients], :from => @options[:sender_address], :subject => subject) do |format|
           format.text { render "#{mailer_name}/background_exception_notification" }
         end.deliver
@@ -97,6 +101,14 @@ class ExceptionNotifier
     end
 
     private
+
+    def process_custom(m, h)
+      @custom_message = m
+      @custom_hash    = h
+      if @custom_hash || @custom_message
+        @sections = ['custom'] + @sections
+      end
+    end
 
     def compose_subject(exception, kontroller=nil)
       subject = "#{@options[:email_prefix]}"
