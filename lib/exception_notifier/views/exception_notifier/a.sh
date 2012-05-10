424ab1adfeff98574a5940f4de9eee3434efb940
Eli Wang <eli@ekohe.com>
2012-05-10 11:03:51 +0800

    Remove a.sh.

--- lib/exception_notifier/views/exception_notifier/a.sh
+++
@@ -1,8 +0,0 @@
-cp _backtrace.text.erb					_backtrace.html.erb				
-cp _data.text.erb					_data.html.erb				
-cp _environment.text.erb				_environment.html.erb				
-cp _request.text.erb					_request.html.erb				
-cp _session.text.erb					_session.html.erb				
-cp _title.text.erb					_title.html.erb				
-cp background_exception_notification.text.erb		background_exception_notification.html.erb	
-cp exception_notification.text.erb			exception_notification.html.erb				
