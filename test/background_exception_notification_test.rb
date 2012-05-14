0204c7017f53dcb0b4d8e61f05af9136b180d747
Eli Wang <eli@ekohe.com>
2012-05-14 14:07:07 +0800

    Update tests. Assertions are based on '@mail.encoded' rather than '@mail.body' to test the multipart mail correctly.

--- test/background_exception_notification_test.rb
+++ test/background_exception_notification_test.rb
@@ -12,6 +12,9 @@ class BackgroundExceptionNotificationTest < ActiveSupport::TestCase
     end
   end
 
+  test "mail should be multipart" do
+  end
+
   test "should have raised an exception" do
     assert_not_nil @exception
   end
@@ -33,18 +36,18 @@ class BackgroundExceptionNotificationTest < ActiveSupport::TestCase
   end
 
   test "mail should say exception was raised in background at show timestamp" do
-    assert @mail.body.include? "A ZeroDivisionError occurred in background at #{@time}"
+    assert @mail.encoded.include? "A ZeroDivisionError occurred in background at #{@time}"
   end
 
   test "mail should contain backtrace in body" do
-    assert @mail.body.include? "test/background_exception_notification_test.rb:6"
+    assert @mail.encoded.include?("test/background_exception_notification_test.rb:6"), "\n#{@mail.inspect}"
   end
 
   test "mail should contain data in body" do
-    assert @mail.body.include? '* data:'
-    assert @mail.body.include? ':payload=>"1/0"'
-    assert @mail.body.include? ':job=>"DivideWorkerJob"'
-    assert @mail.body.include? "My Custom Message"
+    assert @mail.encoded.include? '* data:'
+    assert @mail.encoded.include? ':payload=>"1/0"'
+    assert @mail.encoded.include? ':job=>"DivideWorkerJob"'
+    assert @mail.encoded.include? "My Custom Message"
   end
 
   test "mail should not contain any attachments" do
