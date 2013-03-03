65284753132c5cee42d70c873c3488268757f967
Alex Ghiculescu <alexghiculescu@gmail.com>
2013-03-03 23:27:43 +1000

    fix call to #normalize_digits which was failing on background notifications

--- test/dummy/config/environment.rb
+++ test/dummy/config/environment.rb
@@ -7,6 +7,7 @@ Dummy::Application.config.middleware.use ExceptionNotifier,
   :exception_recipients => %w{dummyexceptions@example.com},
   :email_headers => { "X-Custom-Header" => "foobar" },
   :sections => ['new_section', 'request', 'session', 'environment', 'backtrace'],
+  :normalize_subject => true,
   :background_sections => %w(new_bkg_section) + ExceptionNotifier::Notifier.default_background_sections
 
 # Initialize the rails application
