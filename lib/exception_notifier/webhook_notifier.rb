83a4f888c70cc7a63e75dc3ce510076d286abe0d
Luay Bseiso <luay@buttercloud.com>
2013-07-16 17:41:45 +0300

    Add more values to exception hash in webhook call method

--- lib/exception_notifier/webhook_notifier.rb
+++ lib/exception_notifier/webhook_notifier.rb
@@ -6,14 +6,26 @@ module ExceptionNotifier
     end
 
     def call(exception, options={})
+      env = options[:env]
+
       options = options.reverse_merge(@default_options)
       url = options.delete(:url)
       http_method = options.delete(:http_method) || :post
 
+      request = ActionDispatch::Request.new(env)
+
       options[:body] ||= {}
       options[:body][:exception] = {:error_class => exception.class.to_s,
                                     :message => exception.message.inspect,
-                                    :backtrace => exception.backtrace}
+                                    :backtrace => exception.backtrace,
+                                    :cookies => request.cookies.inspect,
+                                    :url => request.original_url,
+                                    :ip_address => request.ip,
+                                    :environment => env.inspect,
+                                    :controller => env['action_controller.instance'] || MissingController.new,
+                                    :session => env['action_dispatch.request.unsigned_session_cookie'].inspect,
+                                    :parameters => env['action_dispatch.request.parameters'].inspect
+                                  }
 
       HTTParty.send(http_method, url, options)
     end
--- test/exception_notifier/webhook_notifier_test.rb
+++ test/exception_notifier/webhook_notifier_test.rb
@@ -14,6 +14,14 @@ class WebhookNotifierTest < ActiveSupport::TestCase
     assert_equal response[:body][:exception][:error_class], "ZeroDivisionError"
     assert response[:body][:exception][:message].include? "divided by 0"
     assert response[:body][:exception][:backtrace].include? "/exception_notification/test/webhook_notifier_test.rb:48"
+    
+    assert response[:body][:exception][:cookies].has_key?(:cookie_item1)
+    assert_equal response[:body][:exception][:url], "http://example.com/example"
+    assert_equal response[:body][:exception][:ip_address], "192.168.1.1"
+    assert response[:body][:exception][:environment].has_key?(:env_item1)
+    assert_equal response[:body][:exception][:controller], "#<ControllerName:0x007f9642a04d00>"
+    assert response[:body][:exception][:session].has_key?(:session_item1)
+    assert response[:body][:exception][:parameters].has_key?(:controller)
   end
 
   private
@@ -25,7 +33,14 @@ class WebhookNotifierTest < ActiveSupport::TestCase
         :exception => {
           :error_class => 'ZeroDivisionError',
           :message => 'divided by 0',
-          :backtrace => '/exception_notification/test/webhook_notifier_test.rb:48:in `/'
+          :backtrace => '/exception_notification/test/webhook_notifier_test.rb:48:in `/',
+          :cookies => {:cookie_item1 => 'cookieitemvalue1', :cookie_item2 => 'cookieitemvalue2'},
+          :url => 'http://example.com/example',
+          :ip_address => '192.168.1.1',
+          :environment => {:env_item1 => "envitem1", :env_item2 => "envitem2"},
+          :controller => '#<ControllerName:0x007f9642a04d00>',
+          :session => {:session_item1 => "sessionitem1", :session_item2 => "sessionitem2"},
+          :parameters => {:action =>"index", :controller =>"projects"}
         }
       }
     }
