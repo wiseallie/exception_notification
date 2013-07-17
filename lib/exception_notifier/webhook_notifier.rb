e70fc200477a2e3d1c8703f450a4723e55cf40ca
Luay Bseiso <luay@buttercloud.com>
2013-07-17 13:17:57 +0300

    Add support for background notifications in webhook call method

--- lib/exception_notifier/webhook_notifier.rb
+++ lib/exception_notifier/webhook_notifier.rb
@@ -11,21 +11,25 @@ module ExceptionNotifier
       options = options.reverse_merge(@default_options)
       url = options.delete(:url)
       http_method = options.delete(:http_method) || :post
+      
+      unless env.nil? 
+        request = ActionDispatch::Request.new(env)
 
-      request = ActionDispatch::Request.new(env)
+        request_items = {:cookies => request.cookies.inspect,
+                         :url => request.original_url,
+                         :ip_address => request.ip,
+                         :environment => env.inspect,
+                         :controller => env['action_controller.instance'] || MissingController.new,
+                         :session => env['action_dispatch.request.unsigned_session_cookie'].inspect,
+                         :parameters => env['action_dispatch.request.parameters'].inspect}
+      end
 
       options[:body] ||= {}
       options[:body][:exception] = {:error_class => exception.class.to_s,
                                     :message => exception.message.inspect,
                                     :backtrace => exception.backtrace,
-                                    :cookies => request.cookies.inspect,
-                                    :url => request.original_url,
-                                    :ip_address => request.ip,
-                                    :environment => env.inspect,
-                                    :controller => env['action_controller.instance'] || MissingController.new,
-                                    :session => env['action_dispatch.request.unsigned_session_cookie'].inspect,
-                                    :parameters => env['action_dispatch.request.parameters'].inspect,
-                                    :data => (env['exception_notifier.exception_data'] || {}).merge(options[:data] || {}) 
+                                    :data => (env['exception_notifier.exception_data'] || {}).merge(options[:data] || {}),
+                                    :request => (request_items || {})
                                   }
 
       HTTParty.send(http_method, url, options)
