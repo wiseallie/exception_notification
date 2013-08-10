cfa6146ca8d39be7924f92431abbdf32e5629a50
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-08-10 14:37:18 -0400

    Add HipchatNotifier class

--- lib/exception_notifier.rb
+++ lib/exception_notifier.rb
@@ -6,6 +6,7 @@ module ExceptionNotifier
   autoload :Notifier, 'exception_notifier/notifier'
   autoload :EmailNotifier, 'exception_notifier/email_notifier'
   autoload :CampfireNotifier, 'exception_notifier/campfire_notifier'
+  autoload :HipchatNotifier, 'exception_notifier/hipchat_notifier'
   autoload :WebhookNotifier, 'exception_notifier/webhook_notifier'
 
   class UndefinedNotifierError < StandardError; end
