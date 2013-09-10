172d71e7b28b7527793ef0832f6435693a7d2683
Jonhnny Weslley <jw@jonhnnyweslley.net>
2013-09-09 23:34:19 -0300

    only add request itens if env is present

--- lib/exception_notifier/webhook_notifier.rb
+++ lib/exception_notifier/webhook_notifier.rb
@@ -11,8 +11,14 @@ module ExceptionNotifier
       options = options.reverse_merge(@default_options)
       url = options.delete(:url)
       http_method = options.delete(:http_method) || :post
-      
-      unless env.nil? 
+
+      options[:body] ||= {}
+      options[:body][:exception] = {:error_class => exception.class.to_s,
+                                    :message => exception.message.inspect,
+                                    :backtrace => exception.backtrace}
+      options[:body][:data] = (env['exception_notifier.exception_data'] || {}).merge(options[:data] || {})
+
+      unless env.nil?
         request = ActionDispatch::Request.new(env)
 
         request_items = {:cookies => request.cookies.inspect,
@@ -22,16 +28,10 @@ module ExceptionNotifier
                          :controller => env['action_controller.instance'] || MissingController.new,
                          :session => env['action_dispatch.request.unsigned_session_cookie'].inspect,
                          :parameters => env['action_dispatch.request.parameters'].inspect}
+
+        options[:body][:request] = (request_items || {})
       end
 
-      options[:body] ||= {}
-      options[:body][:exception] = {:error_class => exception.class.to_s,
-                                    :message => exception.message.inspect,
-                                    :backtrace => exception.backtrace}
-      
-      options[:body][:request] = (request_items || {})
-      options[:body][:data] = (env['exception_notifier.exception_data'] || {}).merge(options[:data] || {})
-      
       HTTParty.send(http_method, url, options)
     end
   end
