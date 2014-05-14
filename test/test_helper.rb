5fe49a6a423f8adf1900329c61c186d6b1b15eb8
Luciano Sousa <ls@lucianosousa.net>
2014-05-14 12:26:24 -0300

    removing test unit requirement to fix minitest incompatibility

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,7 +12,6 @@ require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require "rails/test_help"
 require File.expand_path("../dummy/test/test_helper.rb", __FILE__)
 
-require "test/unit"
 require "mocha/setup"
 
 Rails.backtrace_cleaner.remove_silencers!
