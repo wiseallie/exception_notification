bd6e176bfabb48b8a60f1b680b17229b1610b040
wiseallie <wiseallie@gmail.com>
2015-06-17 18:45:30 +0200

    Update rails.rb
    require absolute module

--- lib/exception_notification/rails.rb
+++ lib/exception_notification/rails.rb
@@ -1,6 +1,6 @@
 module ExceptionNotification
   class Engine < ::Rails::Engine
-    config.exception_notification = ExceptionNotifier
+    config.exception_notification = ::ExceptionNotifier
     config.exception_notification.logger = Rails.logger
 
     config.app_middleware.use ExceptionNotification::Rack
